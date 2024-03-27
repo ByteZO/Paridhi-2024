f5f054fd8c3a9db563324b3222eedeab8073d4f3
ByteZO <coder.gny@gmail.com>
2024-03-27 22:17:41 +0530

    #18 Refactor Profile component and update user data

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,44 +1,75 @@
 import React from "react";
-import {
-  StyledContainer,
-  Main,
-  ProImg,
-  NameSec,
-  IdSec,
-  RollYearSec,
-  InstituteInfoSec,
-} from "./Profile.styled";
+import { StyledContainer, Domains, MainInfo } from "./Profile.styled";
+import { Card3 } from "../Cards/Card3";
 
-const userData = {
-  profileImage:
-    "https://freerangestock.com/sample/120140/business-man-profile-vector.jpg",
-  name: "Gopaljeet Narayan Yadav",
-  GID: 34563456345645,
-  TID: 23123123123123,
-  rollNo: 31001222086,
-  Year: 2,
-  college: " Meghnad Saha Institute Of Technology",
+const testUserData = {
+  id: 3,
+  name: "soumya dey",
+  college: "msit",
+  year: "3rd",
+  department: "cse",
+  roll: "115",
+  email: "soumyadeep_d.cse2021@msit.edu.in",
+  phoneNumber: "8282866698",
+  gid: "paridhi2000021020521024",
+  megaArchTid: "paridhi12002105202024",
+  setuBandhanTid: "paridhi12002105202024",
+  cp1styearTid: "paridhi12002105202024",
+  cpAllyearTid: "paridhi12002105202024",
+  webTid: "paridhi12002105202024",
+  electrical1Tid: "paridhi12002105202024",
+  electrical2Tid: "paridhi12002105202024",
+  gaming1Tid: "paridhi12002105202024",
+  gaming2Tid: "paridhi12002105202024",
+  gaming3Tid: "paridhi12002105202024",
+  roboraceTid: "paridhi12002105202024",
+  roboSoccerTid: "paridhi12002105202024",
+  roboWar8kgTid: "paridhi12002105202024",
+  roboWar15kgTid: "paridhi12002105202024",
+  lfrtid: "paridhi12002105202024",
+  trackoteasureTid: "paridhi12002105202024",
 };
+
+const userData = [
+  {
+    name: "soumya dey",
+    gID: "paridhi2000021020521024",
+    rollNo: 115,
+    department: "cse",
+    year: "3rd",
+    college: "Meghnad Saha Institute Of Technology",
+  },
+];
+
+
 
 const Profile = () => {
   return (
     <>
       <StyledContainer>
-        <Main>
-          <ProImg $Image={userData.profileImage} />
-          <NameSec>{userData.name}</NameSec>
-          <IdSec>
-            <h1> TID : {userData.TID}</h1>
-            <h1> GID : {userData.GID}</h1>
-          </IdSec>
-          <InstituteInfoSec>
-            <h1>College : {userData.college}</h1>
-          </InstituteInfoSec>
-          <RollYearSec>
-            <h1>Roll No : {userData.rollNo}</h1>
-            <h1>Year : {userData.Year}</h1>
-          </RollYearSec>
-        </Main>
+        <MainInfo>
+          <h1>Name : soumya dey </h1>
+          <h1>GID : paridhi2000021020521024 </h1>
+          <h1>Year : 3rd</h1>
+          <h1>Department : CSE </h1>
+          <h1>Roll No. : 155</h1>
+          <h1>E - Mail : soumyadeep_d.cse2021@msit.edu.in</h1>
+          <h1>Phone : 8282866698</h1>
+          <h1>College : msit </h1>
+        </MainInfo>
+        <Domains> 
+          <h1>CIVIL</h1>
+          <br />
+          <h1>Mega Arch TID : paridhi12002105202024</h1>
+          <h1>Setu Bandhan TID : paridhi12002105202024</h1>
+        </Domains>
+        <Domains> 
+          <h1>CODING</h1>
+          <br />
+          <h1>CP 1st Year TID : paridhi12002105202024</h1>
+          <h1>CP All Year TID : paridhi12002105202024</h1>
+          <h1>WEB TID : paridhi12002105202024</h1>
+        </Domains>
       </StyledContainer>
     </>
   );
--- src/Components/Profile/Profile.styled.js
+++ src/Components/Profile/Profile.styled.js
@@ -4,8 +4,8 @@ export const StyledContainer = styled.section`
   background-image: url(${"https://images.unsplash.com/photo-1603366615917-1fa6dad5c4fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"});
   background-size: cover;
   background-position: center;
-  padding-top: 10vh;
-  height: 100vh;
+  padding-top: 15vh;
+  min-height: 100vh;
   width: 100%;
   display: flex;
   flex-wrap: wrap;
@@ -13,101 +13,38 @@ export const StyledContainer = styled.section`
   justify-content: center;
   align-items: center;
   width: 100%;
+  gap: 20px;
+  flex-wrap: wrap;
 `;
 
-export const Main = styled.section`
-  height: 85%;
-  width: 90%;
-  border: 2px solid white;
-  border-radius: 20px;
+export const MainInfo = styled.div`
+  padding: 20px;
+  height: 27rem;
+  min-width: 20rem;
+  background: rgba(255, 255, 255, 0.25);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(1px);
+  -webkit-backdrop-filter: blur(1px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  color: white;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
-  padding: 10px;
-  gap: 10% 0;
-  color: white;
-  background: rgba(255, 255, 255, 0.15);
+`;
+
+export const Domains = styled.h1`
+  padding: 20px;
+  height: 27rem;
+  width: auto;
+  background: rgba(255, 255, 255, 0.25);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
-  backdrop-filter: blur(1.5px);
-  -webkit-backdrop-filter: blur(1.5px);
-  border-radius: 20px;
-  border: 3px solid whitesmoke;
-
-  @media (max-width: 600px) {
- text-align: center;
-    gap: 9% 0;
-  }
-`;
-export const ProImg = styled.section`
-  width: 10rem;
-
-  height: 10rem;
-  border-radius: 50%;
-  background-image: url(${({ $Image }) => $Image});
-  background-position: center;
-  background-size: contain;
-  @media (max-width: 600px) {
-    width: 5rem;
-    height: 5rem;
-  }
-`;
-export const NameSec = styled.h1`
-  font-size: x-large;
-  letter-spacing: 5px;
-  color: white;
-  border: 10px;
-  @media (max-width: 600px) {
-    letter-spacing: 2px;
-    font-size: large;
-  }
-`;
-export const IdSec = styled.section`
+  backdrop-filter: blur(1px);
+  -webkit-backdrop-filter: blur(1px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
   color: white;
   display: flex;
-  height: 10%;
-  width: 100%;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 20%;
-  flex-wrap: wrap;
-  font-size: x-large;
-
-  @media (max-width: 600px) {
-  
-    gap: 10%;
-    font-size: large;
-  
-  
-  }
-`;
-export const InstituteInfoSec = styled.section`
-  height: 10%;
-  width: 100%;
-  font-size: x-large;
-  flex-wrap: wrap;
-  display: flex;
-  border: 10px;
-
-  justify-content: center;
-  @media (max-width: 600px) {
-    font-size: large;
-  }
-`;
-export const RollYearSec = styled.section`
-  color: white;
-  display: flex;
-  height: 10%;
-  width: 100%;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 25%;
-  border: 10px;
-
-  font-size: x-large;
-  @media (max-width: 600px) {
-    font-size: large;
-    gap:  0 15%;
-
-  }
-`;
+  flex-direction: column;
+  font-size: 1rem;
+`;  
